d411f25abbec32e3b7461a37d5d33a70ec0ab2f4
parkjiung skkusosc <parkjiung321@naver.com>
2024-05-31 14:57:53 +0900

    basic_setting

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import os
 pygame.font.init()
 pygame.mixer.init()
 
+WIDTH= 900
+HEIGHT= 500
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("First Game!")
+
+WHITE = (255,255,255)
+BLACK=(0,0,0)
+RED=(255,0,0)
+YELLOW=(255,255,0)
+
+BORDER=pygame.Rect(WIDTH//2-5,0,10,HEIGHT)
