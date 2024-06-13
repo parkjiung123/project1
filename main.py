7ef79d1990b9d30444095e18dd178dfaa239b61c
minseok1897 <msj8719@naver.com>
2024-06-13 12:07:53 +0900

    Add Start screen and GameOver screen to allow the game to restart
    Signed-off-by: minseok1897 <msj8719@naver.com>

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-import os
+import os, sys
 import random
 
 pygame.font.init()
@@ -14,6 +14,8 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+############### PHASE 2 ###############   시작화면 구성을 위한 색 추가
+SKYBLUE = (153, 255, 255)        
 
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
 
@@ -21,7 +23,9 @@ THROW_SNOW_BALL_SOUND = pygame.mixer.Sound('Source/Throw_Snowball.mp3')
 HIT_SNOW_BALL_SOUND = pygame.mixer.Sound('Source/Hit_Snowball.mp3')
 
 HP_FONT = pygame.font.SysFont('comicsans', 40)
-WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+############### PHASE 2 ###############    시작 화면 및 종료를 위한 폰트 추가
+WINNER_FONT = pygame.font.SysFont('comicsans', 50)
+GAMEOVER_FONT = pygame.font.SysFont('comicsans',100)
 
 FPS = 60
 SPEED = 5
@@ -29,6 +33,10 @@ SNOWBALL_SPEED = 7
 MAX_SNOWBALL = 2
 CHARACTER_WIDTH = 60
 CHARACTER_HEIGHT = 50
+
+############### PHASE 2 ###############    시작 및 재시작 구분 위한 전역변수선언
+winner_text=""
+STATUS=1
 
 BOY_HIT = pygame.USEREVENT + 1
 GIRL_HIT = pygame.USEREVENT + 2
@@ -155,10 +163,13 @@ def draw_winner(text):
     draw_text = WINNER_FONT.render(text, 1, BLACK)
     WIN.blit(draw_text, (WIDTH / 2 - draw_text.get_width() / 2, HEIGHT / 2 - draw_text.get_height() / 2))
     pygame.display.update()
-    pygame.time.delay(5000)
 
 
-def main():
+def runGame():
+    ############### PHASE 2 ############### 게임오버 시 승리 확인 하기위한 변수
+    global winner_text, STATUS
+    STATUS = 2
+    
     girl = pygame.Rect(700, 300, CHARACTER_WIDTH, CHARACTER_HEIGHT)
     boy = pygame.Rect(100, 300, CHARACTER_WIDTH, CHARACTER_HEIGHT)
 
@@ -176,7 +187,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
+                ############### PHASE 2 ###############
+                terminate()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_v and len(boy_snowball) < MAX_SNOWBALL:
@@ -207,8 +219,8 @@ def main():
             winner_text = "Girl Wins!"
 
         if winner_text != "":
-            draw_winner(winner_text)
-            break
+            ############### PHASE 2 ###############
+            run = False
 
         keys_pressed = pygame.key.get_pressed()
         boy_handle_movement(keys_pressed, boy)
@@ -219,6 +231,97 @@ def main():
 
         if random.randint(0, 200) == 0: 
             penguins.append(Penguin(random.randint(0, WIDTH - 30), 0))
+
+
+#######################################
+############### PHASE 2 ###############
+#######################################
+
+#게임 시작화면을 보여주는 showStartScreen 함수
+def showStartScreen():
+    titleFont = pygame.font.Font('freesansbold.ttf', 50)
+    titleSurf1 = titleFont.render('Snowball Game!', True, WHITE, SKYBLUE)
+    titleSurf2 = titleFont.render('Snowball Game!', True, BLACK)
+    pressKeySurf = HP_FONT.render('Press any key to start.', True, BLACK)
+
+    degrees1 = 0
+    while True:
+        WIN.blit(BACKGROUND, (0, 0))
+        
+        rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
+        rotatedRect1 = rotatedSurf1.get_rect()
+        rotatedRect1.center = (WIDTH / 2, HEIGHT / 2)
+        WIN.blit(rotatedSurf1, rotatedRect1)
+
+        titleRect2 = titleSurf2.get_rect()
+        titleRect2.center = (WIDTH / 2, HEIGHT / 2)
+        WIN.blit(titleSurf2, titleRect2)
+
+        pressKeyRect = pressKeySurf.get_rect()
+        pressKeyRect.topleft = (WIDTH / 2 - pressKeyRect.width / 2, HEIGHT - 60)
+        WIN.blit(pressKeySurf, pressKeyRect)
+
+        if checkForKeyPress():
+            pygame.event.get()
+            return
+        pygame.display.update()
+        pygame.time.Clock().tick(FPS)
+        degrees1 += 1
+
+
+#키 누르는 것을 체크하는 checkForKeyPress 함수
+def checkForKeyPress():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_ESCAPE:
+                terminate()
+            return True
+    return False
+
+
+#게임 종료시 승리자를 표시하고 재시작 안내 문구를 알리는 showGameOver함수
+def showGameOver():
+    gameOverSurf = GAMEOVER_FONT.render('Game Over', True, BLACK)
+    gameOverRect = gameOverSurf.get_rect()
+    gameOverRect.midtop = (WIDTH / 2, 50)
+
+    pressKeySurf = HP_FONT.render('Press any key to restart.', True, BLACK)
+    pressKeyRect = pressKeySurf.get_rect()
+    pressKeyRect.topleft = (WIDTH / 2 - pressKeyRect.width / 2, HEIGHT - 60)
+    
+    while True:
+        WIN.blit(BACKGROUND, (0, 0))
+        WIN.blit(gameOverSurf, gameOverRect)
+        WIN.blit(pressKeySurf, pressKeyRect)
+        draw_winner(winner_text)
+        
+
+        if checkForKeyPress():
+            pygame.event.get()
+            return
+
+        pygame.display.update()
+        pygame.time.Clock().tick(FPS)
+
+#게임 종료시 작동되는 terminate 함수
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+# 여러번 진행할 수 있도록 구성된 main 함수
+def main():
+    showStartScreen()
+    while True:
+        runGame()
+        showGameOver()
+
+#######################################
+############### PHASE 2 ###############
+#######################################
+
+
 
 if __name__ == "__main__":
     main()
